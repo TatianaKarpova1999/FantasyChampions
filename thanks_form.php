73c788832445c04e771fe56939c46ebd464a9272
Татьяна <tanya.karpova.ry@gmail.com>
2025-02-19 12:11:52 +0300

    исправляет адаптив

--- thanks_form.php
+++ thanks_form.php
@@ -1,8 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-$name = $_POST["name"];
 $email = $_POST["email"];
-$comment = $_POST["comment"];
 $privacy = $_POST["privacy"];
 
 
@@ -10,9 +8,7 @@ $to = "support@" . $_SERVER['HTTP_HOST'];
 $subject = "New Contact Form Submission";
 
 
-$body = "Name: $name\n";
 $body .= "Email: $email\n";
-$body .= "Comment:\n$comment";
 $body .= "Privacy:\n$privacy";
 
 
